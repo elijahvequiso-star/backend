758400a63aa57220d7e53609ba693a766f311cbb
Prince Vequiso <elijahvequiso@gmail.com>
2026-04-10 18:01:07 +0800

    Backends for capstone1

--- title/title/settings.py
+++ title/title/settings.py
@@ -2,7 +2,11 @@ from pathlib import Path
 import os
 
 import dj_database_url
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    def load_dotenv(*args, **kwargs):
+        return False
 
 
 def env_bool(name, default=False):
